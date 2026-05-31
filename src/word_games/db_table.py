37387c3bde68c0589ecb841246dfc656f9dc45f7
Ziverael <papeernscal@gmail.com>
2026-05-31 22:58:42 +0200

    Add user table

--- src/word_games/db_table.py
+++ src/word_games/db_table.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 
-from sqlalchemy import JSON
+from flask_login import UserMixin
+from sqlalchemy import JSON, String
 from sqlalchemy.orm import Mapped, mapped_column
+from werkzeug.security import check_password_hash, generate_password_hash
 
+from word_games import db
 from word_games.database import BaseTable
+from word_games.error import TokenError
+from word_games.model import Role
+from word_games.utils import TZ_UTC, deserialize, serialize
 
 
 class GameTable(BaseTable):
@@ -21,3 +27,95 @@ class TaskTable(BaseTable):
     created_at: Mapped[datetime]
     viewed_at: Mapped[datetime | None]
     recently_viewed_at: Mapped[datetime | None]
+
+
+class UserTable(BaseTable, UserMixin):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(64), unique=True, index=True)
+    username: Mapped[str] = mapped_column(String(64), unique=True, index=True)
+    role: Mapped[Role]
+    password_hash: Mapped[str] = mapped_column(String(128))
+    confirmed: Mapped[bool] = mapped_column(default=False)
+    last_seen = Mapped[datetime]
+
+    @property
+    def password(self):
+        msg = "password is not a readable attribute"
+        raise AttributeError(msg)
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self):
+        return serialize({"confirm": self.id})
+
+    def confirm(self, token) -> bool:
+        try:
+            data = deserialize(token)
+        except TokenError:
+            return False
+        else:
+            if data.get("confirm") != self.id:
+                return False
+            self.confirmed = True
+            db.session.add(self)
+            return True
+
+    def generate_reset_token(self):
+        return serialize({"reset": self.id})
+
+    @staticmethod
+    def reset_password(token: bytes, new_password: str):
+        try:
+            data = deserialize(token)
+        except TokenError:
+            return False
+        user = UserTable.query.get(data.get("reset"))
+        if user is None:
+            return False
+        user.password = new_password
+        db.session.add(user)
+        return True
+
+    def generate_email_change_token(self, new_email: str):
+        return serialize({"change_email": self.id, "new_email": new_email})
+
+    def change_email(self, token: bytes):
+        try:
+            data = deserialize(token)
+        except TokenError:
+            return False
+        if data.get("change_email") != self.id:
+            return False
+        new_email = data.get("new_email")
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        self.email = new_email
+        self.avatar_hash = self.gravatar_hash()
+        db.session.add(self)
+        return True
+
+    def ping(self):
+        self.last_seen = datetime.now(TZ_UTC)
+        db.session.add(self)
+
+    def generate_auth_token(self):
+        return serialize({"id": self.id})
+
+    @staticmethod
+    def verify_auth_token(token: bytes):
+        try:
+            data = deserialize(token)
+        except TokenError:
+            return None
+        else:
+            return UserTable.query.get(data["id"])
+
+    def __repr__(self):
+        return f"<User {self.username}>"
--- src/word_games/migration.py
+++ src/word_games/migration.py
@@ -4,4 +4,4 @@ Don't import anything from this file.
 """
 # ruff: noqa: F401
 
-from word_games.db_table import GameTable, TaskTable
+from word_games.db_table import GameTable, TaskTable, UserTable
